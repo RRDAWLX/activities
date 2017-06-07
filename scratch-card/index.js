ec2e87737deaaafa1c5004880725eb1ed815ec4e
王龙祥 <longxiang.wang@vipshop.com>
2017-06-07 16:59:49 +0800

    刮刮卡支持手动配置中奖图片

--- scratch-card/index.js
+++ scratch-card/index.js
@@ -1,8 +1,8 @@
 class ScratchCard {
-    constructor(parentSelector, imageUrl) {
+    constructor(parentSelector) {
         this.scratchMobile = this.scratchMobile.bind(this);
         this.scratchPc = this.scratchPc.bind(this);
-        this.checkIfClear = this.checkIfClear.bind(this);
+        this.check = this.check.bind(this);
         this.clear = this.clear.bind(this);
         this.getImageData = this.getImageData.bind(this);
 
@@ -13,8 +13,10 @@ class ScratchCard {
         this.width = this.canvas.width = this.parent.clientWidth;
         this.height = this.canvas.height = this.parent.clientHeight;
 
-        this.canvas.style.background = `url(${imageUrl}) no-repeat center`;
-        this.canvas.style.backgroundSize = 'contain';
+        let style = this.canvas.style;
+        style.backgroundRepeat = 'no-repeat';
+        style.backgroundPosition = 'center';
+        style.backgroundSize = 'contain';
 
         this.cover();
         this.addListeners();
@@ -22,7 +24,7 @@ class ScratchCard {
     }
 
     cover() {
-        this.scratched = false;
+        this.scratched = false;     // 标识未被刮开
         let context = this.context;
         context.globalCompositeOperation = 'source-over';
         context.fillStyle = 'grey';
@@ -31,13 +33,18 @@ class ScratchCard {
 
     }
 
+    setPrizeImage(imgUrl) {
+        this.canvas.style.backgroundImage = `url(${imgUrl})`;
+        return this;
+    }
+
     addListeners() {
         this.canvas.addEventListener('touchstart', this.scratchMobile, false);
         this.canvas.addEventListener('touchmove', this.scratchMobile, false);
         this.canvas.addEventListener('mousemove', this.scratchPc, false);
-        this.canvas.addEventListener('touchend', this.checkIfClear, false);
-        this.canvas.addEventListener('touchcancel', this.checkIfClear, false);
-        this.canvas.addEventListener('mouseout', this.checkIfClear, false);
+        this.canvas.addEventListener('touchend', this.check, false);
+        this.canvas.addEventListener('touchcancel', this.check, false);
+        this.canvas.addEventListener('mouseout', this.check, false);
     }
 
     scratchMobile(e) {
@@ -69,7 +76,7 @@ class ScratchCard {
         context.fill();
     }
 
-    checkIfClear() {
+    check() {
         if (this.scratched) {
             return;
         }
@@ -85,7 +92,7 @@ class ScratchCard {
         if (counter / len * 4 > 0.5) {
             this.clear();
             this.scratched = true;
-            window.alert('中奖了！');
+            setTimeout(() => {window.alert('中奖了！');}, 0);
         }
     }
 
@@ -93,20 +100,41 @@ class ScratchCard {
         this.context.clearRect(0, 0, this.width, this.height);
     }
 
+    transparentize() {
+        let image = this.context.getImageData(0, 0, this.width, this.height);
+        for (let i = 3, len = image.data.length; i <= len; i += 4) {
+            image.data[i] = 0;
+        }
+        this.context.putImageData(image, 0, 0);
+        console.log(image.data.length);
+        console.log(image.data.slice(0, 10));
+    }
+
     getImageData() {
         let data = this.context.getImageData(0, 0, this.width, this.height).data,
             customData = [];
-        for (let i = 0, len = 400; i < len; i += 4) {
+        for (let i = 0, len = 40; i < len; i += 4) {
             customData.push(`(${data[i]}, ${data[i+1]}, ${data[i+2]}, ${data[i+3]})`);
         }
         return customData;
     }
+
+    getPixelLength() {
+        return this.context.getImageData(0, 0, this.width, this.height).data.length / 4;
+    }
 }
 
-let scratchCard = new ScratchCard('#scratchCard', './prize.jpg');
+let scratchCard = new ScratchCard('#scratchCard');
+scratchCard.setPrizeImage('./prize.jpg');
 document.querySelector('#reset').addEventListener('click', () => {
     scratchCard.cover();
 }, false);
+document.querySelector('#trans').addEventListener('click', () => {
+    scratchCard.transparentize();
+}, false);
 document.querySelector('#data').addEventListener('click', () => {
     console.log(scratchCard.getImageData());
+}, false);
+document.querySelector('#info').addEventListener('click', () => {
+    document.querySelector('#msg').textContent = `width * height = ${scratchCard.width} * ${scratchCard.height} = ${scratchCard.width * scratchCard.height}, pixels: ${scratchCard.getPixelLength()}.`;
 }, false);
