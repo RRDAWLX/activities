ae378d2ce920955406ef2477ca8d413eb72fa9b8
王龙祥 <longxiang.wang@vipshop.com>
2017-07-14 14:42:39 +0800

    slot初始化可配置，修复抽奖时总是跳回第一个奖项开始的bug

--- slots/index.js
+++ slots/index.js
@@ -1,79 +1,16 @@
-class Slot {
-    constructor() {
-        this.prizes = [
-            {value: 1, class: 'one'},
-            {value: 2, class: 'two'},
-            {value: 3, class: 'three'}
-        ];
-        if (this.prizes.length < 2) {
-            console.log('Prizes length must equal or greater than 2!');
-            return false;
-        }
-        this.currentPrizeIndex = 2;     // 当前显示奖项序号
-        this.itemHeight = 100;
-        this.totalHeight = this.itemHeight * this.prizes.length;    // 所有奖项总高度
-        this.createDoms();
-        this.getTimingFunctionGenerator();
-    }
-
-    createDoms() {
-        let prizes = this.prizes,
-            slot = this.slot = document.querySelector('.slot'),
-            itemsHtml = '',
-            items = [];
-        items.push(prizes[prizes.length - 1], ...prizes, ...prizes.slice(0, 2));
-        items.forEach(item => {
-            itemsHtml += `<li class="prize ${item.class}">${item.value}</li>`;
-        });
-        slot.innerHTML = `<ul class="prizes-list" style="margin-top: -${this.itemHeight / 2}px; transform: translate3d(0, -${this.itemHeight * this.currentPrizeIndex}px, 0);">${itemsHtml}</ul>`;
-        this.prizesList = this.slot.querySelector('.prizes-list');
-    }
-
-    getTimingFunctionGenerator() {
-        this.timingFunctionGenerator = function(duration) {
-            return d3.easeCubicInOut;
-        };
-    }
-
-    /**
-     * @desc 抽奖，在指定的时间内滚动至指定的奖项。
-     * @param {Number} prizeIndex 奖项序号
-     * @param {Number} duration 摇奖时间，单位 ms
-      */
-    draw({prizeIndex, duration = 8000}) {
-        prizeIndex = prizeIndex % this.prizes.length;
-        let distinedScrollDistance = this.calculateDestinedScrollDistance(prizeIndex);
-        let timingFunction = this.timingFunctionGenerator(duration);
-        let currentPosition = -this.currentPrizeIndex * this.itemHeight;
-        let startTime;
-        let frame = () => {
-            let deltaTime = Date.now() - startTime;
-            if (deltaTime > duration) {
-                deltaTime = duration;
-            }
-            let deltaDistance = distinedScrollDistance * timingFunction(deltaTime / duration);
-            let position = (currentPosition - deltaDistance) % this.totalHeight;
-            this.prizesList.style.transform = `translate3d(0, ${position}px, 0)`;
-            if (deltaTime < duration) {
-                window.requestAnimationFrame(frame);
-            } else {
-                this.currentPosition = prizeIndex;
-            }
-        };
-        startTime = Date.now();
-        frame();
-    }
-
-    /**
-     * @desc 计算出需要滚动的距离
-     */
-    calculateDestinedScrollDistance(prizeIndex) {
-        let distinedDistance = (prizeIndex - this.currentPrizeIndex + 20 * this.prizes.length) * this.itemHeight;
-        return distinedDistance;
-    }
-
-
-}
-
-let slot = new Slot();console.log(slot);
-slot.draw({prizeIndex: 1});
+let slot = new Slot({
+    prizes: [
+        {value: 1, class: 'one'},
+        {value: 2, class: 'two'},
+        {value: 3, class: 'three'}
+    ],
+    loop: 10
+});
+document.querySelector('#slots').appendChild(slot.dom);
+// slot.draw({prizeIndex: 1});
+let prizeSelector = document.querySelector('#prize');
+document.querySelector('#draw').addEventListener('click', function(){
+    slot.draw({
+        prizeIndex: prizeSelector.options[prizeSelector.selectedIndex].value
+    });
+}, false);
---
+++ slots/slot.js
@@ -0,0 +1,77 @@
+class Slot {
+    /**
+     * @param {Array} prizes 奖励数组
+     * @param {Number} itemHeight 单个奖项元素的高度，单位 px，默认 100px。
+     * @param {Number} initialIndex 初始化时显示的奖项,默认为第一个奖项。
+     * @param {Number} loop 抽奖过程中旋转的圈数，默认20圈。
+     */
+    constructor({prizes, itemHeight = 100, initialIndex = 0, loop = 20}) {
+        this.prizes = prizes;
+        if (this.prizes.length < 2) {
+            console.log('Prizes length must equal or greater than 2!');
+            return false;
+        }
+        this.currentPrizeIndex = initialIndex % prizes.length;     // 当前显示奖项序号
+        this.itemHeight = itemHeight;
+        this.totalHeight = this.itemHeight * this.prizes.length;    // 所有奖项总高度
+        this.loop = loop;
+        this.createDoms();
+        this.getTimingFunctionGenerator();
+    }
+
+    createDoms() {
+        let slot = this.dom = document.createElement('div');
+        slot.className = 'slot';
+        let prizes = this.prizes,
+            itemsHtml = '',
+            items = [];
+        items.push(prizes[prizes.length - 1], ...prizes, ...prizes.slice(0, 2));
+        items.forEach(item => {
+            itemsHtml += `<li class="prize ${item.class}">${item.value}</li>`;
+        });
+        slot.innerHTML = `<ul class="prizes-list" style="margin-top: -${this.itemHeight / 2}px; transform: translate3d(0, -${this.itemHeight * this.currentPrizeIndex}px, 0);">${itemsHtml}</ul>`;
+        this.prizesList = slot.querySelector('.prizes-list');
+    }
+
+    getTimingFunctionGenerator() {
+        this.timingFunctionGenerator = function(duration) {
+            return d3.easeCubicInOut;
+        };
+    }
+
+    /**
+     * @desc 计算出需要滚动的距离
+     */
+    calculateDestinedScrollDistance(detinedPrizeIndex) {
+        return (detinedPrizeIndex - this.currentPrizeIndex + this.loop * this.prizes.length) * this.itemHeight;
+    }
+
+    /**
+     * @desc 抽奖，在指定的时间内滚动至指定的奖项。
+     * @param {Number} prizeIndex 奖项序号
+     * @param {Number} duration 摇奖时间，单位 ms
+      */
+    draw({prizeIndex, duration = 8000}) {
+        prizeIndex = prizeIndex % this.prizes.length;
+        let distinedScrollDistance = this.calculateDestinedScrollDistance(prizeIndex);
+        let timingFunction = this.timingFunctionGenerator(duration);
+        let currentPosition = -this.currentPrizeIndex * this.itemHeight;
+        let startTime;
+        let frame = () => {
+            let deltaTime = Date.now() - startTime;
+            if (deltaTime > duration) {
+                deltaTime = duration;
+            }
+            let deltaDistance = distinedScrollDistance * timingFunction(deltaTime / duration);
+            let position = (currentPosition - deltaDistance) % this.totalHeight;
+            this.prizesList.style.transform = `translate3d(0, ${position}px, 0)`;
+            if (deltaTime < duration) {
+                window.requestAnimationFrame(frame);
+            } else {
+                this.currentPrizeIndex = prizeIndex;
+            }
+        };
+        startTime = Date.now();
+        frame();
+    }
+}
