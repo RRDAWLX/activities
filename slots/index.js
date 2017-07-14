6590b3f312319ff299f96fdc3c4a096199b1b869
王龙祥 <longxiang.wang@vipshop.com>
2017-07-14 15:21:21 +0800

    3个老虎机抽奖完成

--- slots/index.js
+++ slots/index.js
@@ -1,16 +1,49 @@
-let slot = new Slot({
-    prizes: [
-        {value: 1, class: 'one'},
-        {value: 2, class: 'two'},
-        {value: 3, class: 'three'}
-    ],
-    loop: 10
+let prizes = [
+    {value: 1, class: 'one'},
+    {value: 2, class: 'two'},
+    {value: 3, class: 'three'}
+];
+
+let slots = [new Slot({prizes}), new Slot({prizes}), new Slot({prizes})];
+slots.forEach(slot => {
+    document.querySelector('#slots').appendChild(slot.dom);
 });
-document.querySelector('#slots').appendChild(slot.dom);
-// slot.draw({prizeIndex: 1});
+
 let prizeSelector = document.querySelector('#prize');
 document.querySelector('#draw').addEventListener('click', function(){
-    slot.draw({
-        prizeIndex: prizeSelector.options[prizeSelector.selectedIndex].value
-    });
+    let prizesArr = getDestinedPrizesArray();
+    slots[0].draw({prizeIndex: prizesArr[0]});
+    setTimeout(() => {
+        slots[1].draw({prizeIndex: prizesArr[1]});
+    }, 1000);
+    setTimeout(() => {
+        slots[2].draw({prizeIndex: prizesArr[2]});
+    }, 2000);
 }, false);
+
+function getDestinedPrizesArray() {
+    let prize = prizeSelector.options[prizeSelector.selectedIndex].value,
+        prizesArr;
+    if (prize != -1) {
+        prizesArr = [];
+        for (let i = 0; i < prizes.length; i++) {
+            prizesArr.push(prize);
+        }
+    } else {
+        let checkIfEqual = val => {
+            return val == prizesArr[0];
+        };
+        do {
+            prizesArr = getRandomArr(prizes.length);
+        } while (prizesArr.every(checkIfEqual));
+    }
+    return prizesArr;
+}
+
+function getRandomArr(length) {
+    let arr = [];
+    for (let i = 0; i < length; i++){
+        arr.push(Math.floor(Math.random() * length));
+    }
+    return arr;
+}
