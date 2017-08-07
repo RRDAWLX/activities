1569a2d9215ea6c38ff872d41f679399340e1907
王龙祥 <longxiang.wang@vipshop.com>
2017-08-07 17:16:48 +0800

    three.js实现地球

--- device-orientation/index.js
+++ device-orientation/index.js
@@ -1,10 +1,13 @@
-try {
 let scene, camera, renderer, axes,
-    sphere, sphereGeometry, sphereMaterial,
+    sphere, sphereGeometry, sphereMaterial, texture,
     cube, cubeGeometry, cubeMaterial,
+    gamma = 0,
     initAlpha, initBeta, initGamma,
-    deltaAphpa = 0, deltaBeta = 0, deltaGamma = 0,
-    touchBeta = 0, touchGamma = 0;
+    deltaAphpa = 0,
+    deltaBeta = 0,
+    deltaGamma = 0,
+    touchBeta = 0,
+    touchGamma = 0;
 
 function init() {
     scene = new THREE.Scene();
@@ -16,20 +19,19 @@ function init() {
     camera.lookAt(scene.position);
 
     renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(new THREE.Color(0x000000));
+    renderer.setClearColor(new THREE.Color(0x111111));
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
     // axes = new THREE.AxisHelper(20);
     // scene.add(axes);
-
-    sphereGeometry = new THREE.SphereGeometry(10, 20, 20);
-    sphereMaterial = new THREE.MeshBasicMaterial({
-        color: 0xff0000,
-        wireframe: true
+    new THREE.TextureLoader().load('./Earth.png', function(texture){
+        sphereGeometry = new THREE.SphereGeometry(10, 40, 40);
+        sphereMaterial = new THREE.MeshBasicMaterial({map: texture});
+        sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+        scene.add(sphere);
+        animate();
     });
-    sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-    scene.add(sphere);
 
     /*cubeGeometry = new THREE.BoxGeometry(10, 10, 10);
     cubeMaterial = new THREE.MeshBasicMaterial({
@@ -41,11 +43,12 @@ function init() {
 }
 
 function animate() {
+    gamma += 0.01;
     // cube.rotation.x = deltaBeta + touchBeta;
     // cube.rotation.y = deltaGamma + touchGamma;
     // cube.rotation.z = deltaAlpha;
     sphere.rotation.x = deltaBeta + touchBeta;
-    sphere.rotation.y = deltaGamma + touchGamma;
+    sphere.rotation.y = gamma + deltaGamma + touchGamma;
     // sphere.rotation.z = deltaAlpha;
     renderer.render(scene, camera);
     window.requestAnimationFrame(animate);
@@ -62,7 +65,17 @@ window.addEventListener('deviceorientation', e => {
     deltaGamma = (e.gamma - initGamma) / 180 * Math.PI * 2;
 });
 
-/*let startX, startY;
+// devicemotion事件对象的相关属性还未理解清楚
+/*var t = Date.now();
+var p = document.querySelector('p');
+window.addEventListener('devicemotion', e => {
+    if (Date.now() - t > 1000) {
+        t = Date.now();
+        p.innerHTML = `${e.acceleration.x}<br/>${e.acceleration.y}<br/>${e.acceleration.z}`;
+    }
+});*/
+
+let startX, startY;
 window.addEventListener('touchstart', e => {
     startX = e.touches[0].clientX;
     startY = e.touches[0].clientY;
@@ -72,10 +85,6 @@ window.addEventListener('touchmove', e => {
         deltaY = e.changedTouches[0].clientY - startY;
     touchBeta += deltaY / 1440;
     touchGamma += deltaX / 1440;
-});*/
+});
 
 init();
-animate();
-} catch (e) {
-    alert(e);
-}
