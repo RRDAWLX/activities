e99ba3d0bdc89c060c5525ab61c50f546b7962c6
longxiang.wang <longxiang.wang@vipshop.com>
2018-03-07 11:49:51 +0800

    支持触屏滑动查看全景

--- panorama/index.js
+++ panorama/index.js
@@ -1,15 +1,11 @@
 /**
  * 全景照片
+ * 测试发现 camera 中旋转相关的方法都无效
  */
 
 let scene, camera, renderer,
-    gamma = 0,
-    initAlpha, initBeta, initGamma,
-    deltaAphpa = 0,
-    deltaBeta = 0,
-    deltaGamma = 0,
-    touchBeta = 0,
-    touchGamma = 0;
+    a = 0,  // 空间中点P在Oxz屏幕上的射影Q，迎着y轴看时，OQ按逆时针方向旋转与z轴正向的夹角。
+    b = Math.PI / 2;  // OQ与y轴正向的夹角，定义域为 [0, π]。
 
 function init() {
     scene = new THREE.Scene();
@@ -18,7 +14,6 @@ function init() {
     camera.position.x = 0;
     camera.position.y = 0;
     camera.position.z = 0;
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(new THREE.Color(0x111111));
@@ -66,43 +61,37 @@ function createCubeMap() {
 }
 
 function render() {
-  let now = Date.now() / 1000 / 60 * Math.PI * 2;
-  camera.lookAt(new THREE.Vector3(Math.sin(now), 0, Math.cos(now)))
+  camera.lookAt(new THREE.Vector3(Math.sin(b) * Math.sin(a), Math.cos(b), Math.sin(b) * Math.cos(a)));
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 }
 
-/*window.addEventListener('deviceorientation', e => {
-    if (initAlpha === undefined) {
-        initAlpha = e.alpha;
-        initBeta = e.beta;
-        initGamma = e.gamma;
-    }
-    deltaAlpha = -(e.alpha - initAlpha) / 180 * Math.PI;
-    deltaBeta = (e.beta - initBeta) / 180 * Math.PI;
-    deltaGamma = (e.gamma - initGamma) / 180 * Math.PI * 2;
-});*/
-
-// devicemotion事件对象的相关属性还未理解清楚
-/*var t = Date.now();
-var p = document.querySelector('p');
-window.addEventListener('devicemotion', e => {
-    if (Date.now() - t > 1000) {
-        t = Date.now();
-        p.innerHTML = `${e.acceleration.x}<br/>${e.acceleration.y}<br/>${e.acceleration.z}`;
-    }
-});*/
-
-/*let startX, startY;
+let startX, startY, preA, preB;
 window.addEventListener('touchstart', e => {
     startX = e.touches[0].clientX;
     startY = e.touches[0].clientY;
+    preA = a;
+    preB = b;
 });
+
+let factor = Math.PI / 180 / 10;
 window.addEventListener('touchmove', e => {
     let deltaX = e.changedTouches[0].clientX - startX,
         deltaY = e.changedTouches[0].clientY - startY;
-    touchBeta += deltaY / 1440;
-    touchGamma += deltaX / 1440;
+    a = preA + deltaX * factor;
+    let tempB = preB - deltaY * factor;
+    // 当 camera 的视线平行于 y 轴时，需对其姿态做调整，但目前看来无法调整其姿态，所以让其视线永远无法平行于 y 轴。
+    if (tempB <= 0) {
+      b = 1e-10;
+    } else if (tempB >= Math.PI) {
+      b = Math.PI - 1e-10;
+    } else {
+      b = tempB;
+    }
+});
+
+/*window.addEventListener('touchend', e => {
+  console.log(`(${a / Math.PI * 180}, ${b / Math.PI * 180}), (${Math.sin(b) * Math.sin(a)}, ${Math.cos(b)}, ${Math.sin(b) * Math.cos(a)})`)
 });*/
 
 init();
