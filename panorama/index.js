1af5dd065228cc5473d9aa0dcc9c1b5a88c0441a
rrdawlx <815311850@qq.com>
2023-02-08 17:38:52 +0800

    全景照片：优化代码

--- panorama/index.js
+++ panorama/index.js
@@ -6,8 +6,6 @@ import * as THREE from 'three'
 let scene
 let camera
 let renderer
-let a = 0  // 空间中点P在Oxz屏幕上的射影Q，迎着y轴看时，OQ按逆时针方向旋转与z轴正向的夹角。
-let b = Math.PI / 2  // OQ与y轴正向的夹角，定义域为 [0, π]。
 
 function init() {
   scene = new THREE.Scene()
@@ -18,7 +16,7 @@ function init() {
   camera.position.z = 0
 
   renderer = new THREE.WebGLRenderer()
-  renderer.setClearColor(new THREE.Color(0x111111))
+  // renderer.setClearColor(new THREE.Color(0x111111))
   renderer.setSize(window.innerWidth, window.innerHeight)
   document.body.appendChild(renderer.domElement)
 
@@ -50,32 +48,34 @@ function init() {
   render()
 }
 
+let a = 0  // 空间中点P在Oxz屏幕上的射影Q，迎着y轴看时，OQ按逆时针方向旋转与z轴正向的夹角。
+let b = Math.PI / 2  // OP与y轴正向的夹角，定义域为 [0, π]。
 function render() {
+  // sin(b) 是单位向量在 Oxz 平面上投影的长度
   camera.lookAt(new THREE.Vector3(Math.sin(b) * Math.sin(a), Math.cos(b), Math.sin(b) * Math.cos(a)))
   renderer.render(scene, camera)
   requestAnimationFrame(render)
 }
 
-let startX, startY, preA, preB
+let preX
+let preY
 function startDragging(x, y) {
-  startX = x
-  startY = y
-  preA = a
-  preB = b
+  preX = x
+  preY = y
 }
-let factor = Math.PI / 180 / 10
+
+let factor = Math.PI / 180 / 200
 function drag(x, y) {
-  let deltaX = x - startX
-  let deltaY = y - startY
-  a = preA + deltaX * factor
-  let tempB = preB - deltaY * factor
+  let deltaX = x - preX
+  let deltaY = y - preY
+  a += deltaX * factor
+  b -= deltaY * factor
+
   // camera 沿着水平轴旋转超过 90° 或 -90° 后画面会发生上下颠倒，不符合需求，所以让其视线永远无法平行于 y 轴。
-  if (tempB <= 0) {
+  if (b <= 0) {
     b = 1e-10
-  } else if (tempB >= Math.PI) {
+  } else if (b >= Math.PI) {
     b = Math.PI - 1e-10
-  } else {
-    b = tempB
   }
 }
 
