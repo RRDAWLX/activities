3da611869ea54b061bf1ec05a981692259c03d21
rrdawlx <815311850@qq.com>
2023-02-08 16:54:03 +0800

    格式化全景照片代码

--- panorama/index.js
+++ panorama/index.js
@@ -1,61 +1,62 @@
 /**
  * 全景照片
- * 测试发现 camera 中旋转相关的方法都无效
  */
-import * as THREE from 'three';
+import * as THREE from 'three'
 
-let scene, camera, renderer,
-    a = 0,  // 空间中点P在Oxz屏幕上的射影Q，迎着y轴看时，OQ按逆时针方向旋转与z轴正向的夹角。
-    b = Math.PI / 2;  // OQ与y轴正向的夹角，定义域为 [0, π]。
+let scene
+let camera
+let renderer
+let a = 0  // 空间中点P在Oxz屏幕上的射影Q，迎着y轴看时，OQ按逆时针方向旋转与z轴正向的夹角。
+let b = Math.PI / 2  // OQ与y轴正向的夹角，定义域为 [0, π]。
 
 function init() {
-    scene = new THREE.Scene();
+  scene = new THREE.Scene()
 
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 0;
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+  camera.position.x = 0
+  camera.position.y = 0
+  camera.position.z = 0
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(new THREE.Color(0x111111));
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+  renderer = new THREE.WebGLRenderer()
+  renderer.setClearColor(new THREE.Color(0x111111))
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
 
-    let textureCube = new THREE.CubeTextureLoader()
-      .setPath('./').
-      load([
-        './posx.jpg',
-        './negx.jpg',
-        './posy.jpg',
-        './negy.jpg',
-        './posz.jpg',
-        './negz.jpg',
-      ]);
+  let textureCube = new THREE.CubeTextureLoader()
+    .setPath('./').
+    load([
+      './posx.jpg',
+      './negx.jpg',
+      './posy.jpg',
+      './negy.jpg',
+      './posz.jpg',
+      './negz.jpg',
+    ])
 
-    let shader = THREE.ShaderLib.cube;
-    shader.uniforms.tCube.value = textureCube;
+  let shader = THREE.ShaderLib.cube
+  shader.uniforms.tCube.value = textureCube
 
-    let material = new THREE.ShaderMaterial({
-      fragmentShader: shader.fragmentShader,
-      vertexShader: shader.vertexShader,
-      uniforms: shader.uniforms,
-      depthWrite: false,
-      side: THREE.DoubleSide
-    });
+  let material = new THREE.ShaderMaterial({
+    fragmentShader: shader.fragmentShader,
+    vertexShader: shader.vertexShader,
+    uniforms: shader.uniforms,
+    depthWrite: false,
+    side: THREE.DoubleSide
+  })
 
-    let panoramaCube = new THREE.Mesh(new THREE.BoxGeometry(1000, 1000, 1000), material);
-    scene.add(panoramaCube);
+  let panoramaCube = new THREE.Mesh(new THREE.BoxGeometry(1000, 1000, 1000), material)
+  scene.add(panoramaCube)
 
-    render();
+  render()
 }
 
 function render() {
-  camera.lookAt(new THREE.Vector3(Math.sin(b) * Math.sin(a), Math.cos(b), Math.sin(b) * Math.cos(a)));
-  renderer.render(scene, camera);
-  requestAnimationFrame(render);
+  camera.lookAt(new THREE.Vector3(Math.sin(b) * Math.sin(a), Math.cos(b), Math.sin(b) * Math.cos(a)))
+  renderer.render(scene, camera)
+  requestAnimationFrame(render)
 }
 
-let startX, startY, preA, preB;
+let startX, startY, preA, preB
 function startDragging(x, y) {
   startX = x
   startY = y
@@ -88,7 +89,7 @@ window.addEventListener('touchmove', e => {
 
 /*window.addEventListener('touchend', e => {
   console.log(`(${a / Math.PI * 180}, ${b / Math.PI * 180}), (${Math.sin(b) * Math.sin(a)}, ${Math.cos(b)}, ${Math.sin(b) * Math.cos(a)})`)
-});*/
+})*/
 
 window.addEventListener('mousedown', e => {
   startDragging(e.clientX, e.clientY)
@@ -101,4 +102,4 @@ window.addEventListener('mousedown', e => {
   }, { once: true })
 })
 
-init();
+init()
